47325cf0df8a36cb00d369e61dced40d19b3ffce
wolfgangfoulkes <wolfgangfoulkes@alum.calarts.edu>
2014-05-21 16:17:47 -0700

    created a project based on the “meshes” tutorial

---
+++ GLVertex/xcode/build/GLVertex.build/Debug/GLVertex.build/Objects-normal/i386/CinderProjectApp.d
@@ -0,0 +1,17 @@
+dependencies: \
+  /Users/wolfgag/cinderProjects/vertexAndNormals/GLVertex/src/CinderProjectApp.cpp \
+  ../../../../cinder_0.8.5_mac/include/cinder/app/AppNative.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/gl/Texture.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/TriMesh.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/ObjLoader.h \
+  ../../../../cinder_0.8.5_mac/boost/boost/tuple/tuple_comparison.hpp \
+  ../../../../cinder_0.8.5_mac/boost/boost/tuple/tuple.hpp \
+  ../../../../cinder_0.8.5_mac/boost/boost/tuple/detail/tuple_basic.hpp \
+  ../../../../cinder_0.8.5_mac/boost/boost/type_traits/cv_traits.hpp \
+  ../../../../cinder_0.8.5_mac/boost/boost/type_traits/add_cv.hpp \
+  /Users/wolfgag/cinderProjects/vertexAndNormals/GLVertex/xcode/../../../../cinder_0.8.5_mac/boost/boost/type_traits/detail/type_trait_def.hpp \
+  /Users/wolfgag/cinderProjects/vertexAndNormals/GLVertex/xcode/../../../../cinder_0.8.5_mac/boost/boost/type_traits/detail/template_arity_spec.hpp \
+  /Users/wolfgag/cinderProjects/vertexAndNormals/GLVertex/xcode/../../../../cinder_0.8.5_mac/boost/boost/type_traits/detail/type_trait_undef.hpp \
+  ../../../../cinder_0.8.5_mac/boost/boost/type_traits/remove_volatile.hpp \
+  /Users/wolfgag/cinderProjects/vertexAndNormals/GLVertex/xcode/vboClass.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/gl/Vbo.h
---
+++ GLVertex/xcode/build/GLVertex.build/Debug/GLVertex.build/Objects-normal/i386/vboClass.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/wolfgag/cinderProjects/vertexAndNormals/GLVertex/xcode/vboClass.cpp \
+  /Users/wolfgag/cinderProjects/vertexAndNormals/GLVertex/xcode/vboClass.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/app/AppNative.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/gl/Texture.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/gl/Vbo.h \
+  ../../../../cinder_0.8.5_mac/include/cinder/TriMesh.h
